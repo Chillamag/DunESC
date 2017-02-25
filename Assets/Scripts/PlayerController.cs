6236f8197c5b42b5c5c7869fe03f540593bdee39
Chillamag <chillamag@hotmail.com>
2017-02-25 22:02:38 +0100

    First

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Vector3 dir; 
+
+	// Use this for initialization
+	void Start () {
+        dir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            if(dir == Vector3.back) {
+                dir = Vector3.right;
+            }else {
+                dir = Vector3.back;
+            }
+        }
+
+        float amountToMove = speed * Time.deltaTime;
+
+        transform.Translate(dir * amountToMove);
+
+	}
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public GameObject[] tilePrefabs;
+
+    public GameObject currentTile;
+
+    //Start singleton
+    private static TileManager instance;
+    public static TileManager Instance {
+        get {
+
+            if(instance == null) {
+                instance = GameObject.FindObjectOfType<TileManager>();
+            }
+            return instance;
+        }
+    } //End singleton
+
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < 20; i++) {
+            SpawnTile();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnTile() {
+
+        int randomIndex = Random.Range(0, 2);
+
+        currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex], currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+    }
+}
