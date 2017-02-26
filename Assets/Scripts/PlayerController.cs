5f4ba8f316ed319c8cfd2b97bdd4c8f288efb330
Chillamag <chillamag@hotmail.com>
2017-02-26 02:01:27 +0100

    Added Jim 0.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,23 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
 
-    private Vector3 dir; 
+    private Vector3 dir;
+    private SpriteRenderer mySpriteRenderer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         dir = Vector3.zero;
-	}
+        mySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
             if(dir == Vector3.back) {
+                mySpriteRenderer.flipX = true;
                 dir = Vector3.right;
             }else {
+                mySpriteRenderer.flipX = false;
                 dir = Vector3.back;
             }
         }
